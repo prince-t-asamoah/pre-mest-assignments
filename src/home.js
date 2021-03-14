397fb0209422870f7b07f8800fed7c432109c301
PRINCE TWUMASI ASAMOAH <princetwumasam@gmail.com>
2021-03-14 20:57:29 +0000

    My first commit

---
+++ src/home.js
@@ -0,0 +1,11 @@
+import Navbar from './navbar';
+
+function Home() {
+    return(
+        <div>
+            <Navbar/>
+            <h2>This is my homepage</h2>
+        </div>
+    )
+}
+export default Home;
